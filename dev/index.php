4ac4279653fc93a58b81ba14282c95f60810e110
Klvst3r <klvst3r@yahoo.com>
2020-02-04 19:44:53 -0600

    5.6. Split dashboard and crate template dev

---
+++ dev/index.php
@@ -0,0 +1,25 @@
+<?php
+require "inc/head.html";
+?>
+<?php
+require "inc/sidebar.html";
+?>
+ 
+   <div class="main-panel" id="main-panel">
+      <!-- Navbar -->
+<?php
+require "inc/navbar.html";
+?>
+      <!-- End Navbar -->      
+<?php
+require "inc/maincontent.html";
+?>       
+
+</div>
+
+<?php
+require "inc/footer.html";
+require "inc/core-js.html";
+?>      
+
+</body></html>
