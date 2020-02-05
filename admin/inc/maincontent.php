7283146df5c4a4fb7819fd14aa61e75c9b401d36
Klvst3r <klvst3r@yahoo.com>
2020-02-05 13:46:34 -0600

    6.6. Split Dashboard Files Admin

---
+++ admin/inc/maincontent.php
@@ -0,0 +1,212 @@
+
+
+
+<div class="content">
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-header card-header-primary">
+              <h4 class="card-title">Listado</h4>
+              <p class="card-category">Usuarios</p>
+            </div>
+            <div class="card-body">
+
+
+
+
+
+
+              <!-- <div id="typography">
+                <div class="card-title">
+                  <h2>Typography</h2>
+                </div>
+                <div class="row">
+                  <div class="tim-typo">
+                    <h3>
+                      <span class="tim-note">Header 3</span> The Life of Material Dashboard</h3>
+                  </div>
+                  
+                  <div class="tim-typo">
+                    <p>
+                      <span class="tim-note">Paragraph</span>
+                      I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.</p>
+                  </div>
+                  <div class="tim-typo">
+                    <span class="tim-note">Quote</span>
+                    <blockquote class="blockquote">
+                      <p>
+                        I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.
+                      </p>
+                      <small>
+                        Kanye West, Musician
+                      </small>
+                    </blockquote>
+                  </div>
+                  <div class="tim-typo">
+                    <span class="tim-note">Muted Text</span>
+                    <p class="text-muted">
+                      I will be the leader of a company that ends up being worth billions of dollars, because I got the answers ...
+                    </p>
+                  </div>
+                  <div class="tim-typo">
+                    <span class="tim-note">Primary Text</span>
+                    <p class="text-primary">
+                      I will be the leader of a company that ends up being worth billions of dollars, because I got the answers... </p>
+                  </div>
+                  <div class="tim-typo">
+                    <span class="tim-note">Info Text</span>
+                    <p class="text-info">
+                      I will be the leader of a company that ends up being worth billions of dollars, because I got the answers... </p>
+                  </div>
+                  <div class="tim-typo">
+                    <span class="tim-note">Success Text</span>
+                    <p class="text-success">
+                      I will be the leader of a company that ends up being worth billions of dollars, because I got the answers... </p>
+                  </div>
+                  <div class="tim-typo">
+                    <span class="tim-note">Warning Text</span>
+                    <p class="text-warning">
+                      I will be the leader of a company that ends up being worth billions of dollars, because I got the answers...
+                    </p>
+                  </div>
+                  <div class="tim-typo">
+                    <span class="tim-note">Danger Text</span>
+                    <p class="text-danger">
+                      I will be the leader of a company that ends up being worth billions of dollars, because I got the answers... </p>
+                  </div>
+                  
+
+
+                </div>
+              </div> -->
+
+
+
+
+
+
+
+              <section class="invoice">
+  <!-- title row -->
+  <div class="">
+    <div class="col-xs-12">
+      <div class="">
+        <div class="card-category">
+
+          <center>
+          
+
+          <!-- <h3 class="page-header">
+            <i class="material-icons">arrow_forward_ios </i> Registro de Documentos </h3>  -->
+
+<div class="card-body">
+  <div class="table-responsive">
+
+
+<?php
+
+
+//$query = "SELECT id_user as ID, name, user_name FROM users";
+$params = "";
+
+//UserDAO::getTableUsers($params);
+UserController::getTableUsers($params);
+
+
+//modal
+include 'modalGeneral.php';
+
+
+
+?>  
+
+
+
+
+ <!--         
+    <table class="table">
+        <thead class=" text-primary">
+            <tr>
+                <th>
+                    Nombre
+                </th>
+                <th>
+                    Usuario
+                </th>
+                
+                <th class="text-right">
+                    Detalle
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                  N
+                </td>
+                <td>
+                    Niger
+                </td>
+                
+                <td class="text-right">
+                    $36,738
+                </td>
+            </tr>
+            
+        </tbody>
+    </table>
+-->
+
+</div>
+
+        </div>
+
+
+
+      </div>
+      
+    </div>
+    <!-- /.col -->
+  </div>
+
+
+  <div class="row">
+      
+  </div>
+
+
+
+<!--   <div class="row invoice-info">
+  <div class="col-xs-12">
+      <p class="lead text-center">Registro</p>
+    </div>
+    
+
+
+  </div> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</section>
+
+
+
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
