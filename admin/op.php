080b296dab1062f65540cb9815138c501d43f416
Klvst3r <klvst3r@yahoo.com>
2020-02-12 17:25:56 -0600

    Add new options in menu

---
+++ admin/op.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+//$_SESSION["user"]["code"] = "true";
+
+$id = $_GET['id'];
+/**
+ * Dentro de cada opcion debera cargarse las variables de session para quedar dentro del sistema,
+ * este valor sera integrado despues de cargar el menu
+  */
+if(isset($id)){
+	switch($id){
+		case 1:
+			//Listar usuarios
+			/*require_once("view/listUser.php");*/
+			break;
+		case 2:
+			//Registrar Usuarios
+			require_once("view/formRegUser.php");
+			break;
+		case 3:
+		//Capturar valores del form
+			require_once("view/regUser.php");
+			break;
+		case 11:
+			//Modificar Usuarios
+			require_once("view/editUser.php");
+			break;
+		case 21:
+			//Eliminar Usuiarios
+			require_once("view/deleteUser.php");
+			break;
+		case 31:
+			//Cambiar Pass
+			require_once("view/changePass.php");
+			break;
+		
+		default:
+			echo"No hay mas Opciones";
+			echo "<br/>";
+			echo"<meta HTTP-EQUIV='Refresh' CONTENT='3; URL=../index.php'<head/>";
+			break;
+
+	}
+}
+
+?>
