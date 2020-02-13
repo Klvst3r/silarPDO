dca880fddd604d805c9f640fe445f2d5617fdfaa
Klvst3r <klvst3r@yahoo.com>
2020-02-12 19:38:34 -0600

    8.6. Apply security on session in views

--- admin/index.php
+++ admin/index.php
@@ -1,27 +1,37 @@
 <?php
+session_start();
+
 include '../controller/UserController.php';
 
 require "inc/head.html";
-?>
-<?php
-require "inc/sidebar.html";
-?>
- 
-   <div class="main-panel">
-      <!-- Navbar -->
-<?php
-require "inc/navbar.html";
-?>
-      <!-- End Navbar -->
-<?php
-require "inc/maincontent.php";
-?>   
-</div>
 
-<?php
-//require "inc/footer.html";
-//require "inc/fixed-plugin.html";
-require "inc/core-js.html";
+if(isset($_SESSION['user'])){
+
+	?>
+	<?php
+	require "inc/sidebar.html";
+	?>
+	 
+	   <div class="main-panel">
+	      <!-- Navbar -->
+	<?php
+	require "inc/navbar.html";
+	?>
+	      <!-- End Navbar -->
+	<?php
+	require "inc/maincontent.php";
+	?>   
+	</div>
+
+	<?php
+	//require "inc/footer.html";
+	//require "inc/fixed-plugin.html";
+	require "inc/core-js.html";
+
+}else{
+  echo '<script>location.href = "../index.php"</script>';
+}
+
 ?>      
 
 </body></html>
