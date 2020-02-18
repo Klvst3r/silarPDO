7818747a3f81003200c5801994689e457bbe5a33
Klvst3r <klvst3r@yahoo.com>
2020-02-18 15:24:39 -0600

    11.4. Get info in Table for Users

--- controller/UserController.php
+++ controller/UserController.php
@@ -80,4 +80,44 @@ class UserController{
 
 	}//regUser method
 
+	//User Listo for edit
+	public function getTableUsersEdit($user){
+		$obj_user = new User();
+
+		$obj_user->setId_user($user);
+
+		return UserDAO::getTableUsersEdit($obj_user);
+	
+	}//getTableUsersEdit method
+
+	//Obtain User Data for Edit
+	public function getUserData($user){
+
+		$obj_user = new User();
+
+		$obj_user->setId_user($user);
+
+		//echo $obj_user->getId_user();
+
+		return UserDAO::getUserData($obj_user);
+
+	}///getDoc method
+
+	public function updateUser($id_user, $id_priv, $id_status_user, $name, $user_name, $user_tel, $user_email, $user_position){
+		$obj_user = new User();
+
+		$obj_user->setId_user($id_user);
+		$obj_user->setId_priv($id_priv);
+		$obj_user->setId_status_user($id_status_user);
+		$obj_user->setName($name);
+		$obj_user->setUser_name($user_name);
+		$obj_user->setUser_tel($user_tel);
+		$obj_user->setUser_email($user_email);
+		$obj_user->setUser_position($user_position);
+
+		return UserDAO::updateUser($obj_user);
+
+	}//regUser method
+	
+
 }//Class UserController	
