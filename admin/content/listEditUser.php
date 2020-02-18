a1a6b4b551a121a38a18f7f8fbbe00d9143181f6
Klvst3r <klvst3r@yahoo.com>
2020-02-18 14:48:04 -0600

    10.4. Create list user edition

---
+++ admin/content/listEditUser.php
@@ -0,0 +1,71 @@
+
+<div class="content">
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-header card-header-primary">
+              <h4 class="card-title">Listado de Edición</h4>
+              <p class="card-category">Usuarios</p>
+            </div>
+            <div class="card-body">
+
+
+<div class="card-body">
+  <div class="row">
+  <div class="">
+    <h4 class="card-title">Seleccione el usuario a editar</h4>
+  </div>
+</div>
+  
+
+
+<section class="invoice">
+  <!-- title row -->
+  <div class="">
+    <div class="col-xs-12">
+      <div class="">
+        <div class="card-category">
+
+          <center>
+          
+  <div class="card-body">
+  <div class="table-responsive">
+
+<?php
+
+$params = "";
+
+
+UserController::getTableUsersEdit($params);
+
+?>  
+
+
+</div>
+
+        </div>
+
+
+
+      </div>
+      
+    </div>
+    <!-- /.col -->
+  </div>
+
+
+  <div class="row">
+      
+  </div>
+
+
+</section>
+
+
+
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
